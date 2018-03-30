b9e917df4967737e37005cfa5ca153983a236376
Sagar Shah <sagarshah2326@gmail.com>
2018-03-30 08:57:49 +0000

    garlic: Update PowerHAL implementation
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- device.mk
+++ device.mk
@@ -300,7 +300,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service 
 
 # QMI
 PRODUCT_PACKAGES += \
