96ec94bcca0856836961cd20ba284859298be2a3
Sagar Shah <sagarshah2326@gmail.com>
2018-05-12 14:26:25 -0400

    Revert "garlic: rootdir: Redirect working services from system/bin to vendor/bin"
    This reverts commit 129669358160122463c08778a551e7411f57836e.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -43,14 +43,14 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 write_headers
 
 # The standard blobs
-write_makefiles "$MY_DIR"/proprietary-files.txt true
+write_makefiles "$MY_DIR"/proprietary-files.txt
 
 # Qualcomm BSP blobs - we put a conditional around here
 # in case the BSP is actually being built
 printf '\n%s\n' "ifeq (\$(QCPATH),)" >> "$PRODUCTMK"
 printf '\n%s\n' "ifeq (\$(QCPATH),)" >> "$ANDROIDMK"
 
-write_makefiles "$MY_DIR"/proprietary-files-qc.txt true
+write_makefiles "$MY_DIR"/proprietary-files-qc.txt
 
 printf '%s\n' "endif" >> "$PRODUCTMK"
 printf '%s\n' "endif" >> "$ANDROIDMK"
